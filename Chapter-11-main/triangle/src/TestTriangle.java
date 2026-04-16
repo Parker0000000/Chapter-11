bb410ab0924fc03644db08791189c6f021403b2f
Parker0000000 <flowers.par8470@stu.stech.edu>
2026-04-16 10:15:10 -0600

    finished triangle

--- Chapter-11-main/triangle/src/TestTriangle.java
+++ Chapter-11-main/triangle/src/TestTriangle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TestTriangle {
     public static void main(String[] args) throws Exception {
         String color = "red";
@@ -5,6 +7,24 @@ public class TestTriangle {
         double Side1 = 10;
         double Side2 = 10;
         double Side3 = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Side 1:");
+        Side1 = input.nextInt();
+        System.out.println("Enter Side 2:");
+        Side2 = input.nextInt();
+        System.out.println("Enter Side 3:");
+        Side3 = input.nextInt();
+        System.out.println("Is the triangle filled?");
+        String j = input.next();
+        if (j.equalsIgnoreCase("no")) {
+            filled = false;
+        } else {
+            filled = true;
+        }
+        System.out.println("Enter color of triangle:");
+        color = input.next();
+        
+        
         
         Triangle triangle = new Triangle(Side1, Side2, Side3);
         GeometricObject geoObj = new GeometricObject(color, filled);
--- Chapter-11-main/triangle/src/Triangle.java
+++ Chapter-11-main/triangle/src/Triangle.java
@@ -1,30 +1,33 @@
 public class Triangle extends GeometricObject{
-private double Side1 = 1.0;
-private double Side2 = 1.0;
-private double Side3 = 1.0;
+private double Side10 = 1.0;
+private double Side20 = 1.0;
+private double Side30 = 1.0;
+private double area;
 public Triangle() {}
 
 
 
 
 public Triangle(double Side1, double Side2, double Side3) {
- this.Side1 = Side1;
- this.Side2 = Side2;
- this.Side3 = Side3;
+ this.Side10 = Side1;
+ this.Side20 = Side2;
+ this.Side30 = Side3;
+ double s = (Side10 + Side20 + Side30) / 2.0;
+ this.area = Math.sqrt(s*(s-Side10)*(s-Side20)*(s-Side30));
 }
 
 
 double getPerimiter() {
-    return Side1 + Side2 + Side3;
+    return Side10 + Side20 + Side30;
 }
 
 String ToString() {
-return "Triangle: Side1 = " + Side1 + " + Side2 " + Side2 + " + Side3 " + Side3;
+return "Triangle: Side1 = " + Side10 + " + Side2 " + Side20 + " + Side3 " + Side30;
 }
 
 double getArea() {
-    double s = (Side1 + Side2 + Side3) / 2;
-    return Math.sqrt(s*(s-Side1)*(s-Side2)*(s-Side3));
+    
+    return area;
 }
 }
 
