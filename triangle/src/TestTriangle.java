893e9c52da419040523eeaaba1c4ed4376141355
Parker0000000 <flowers.par8470@stu.stech.edu>
2026-04-21 09:33:57 -0600

    Fixing things

--- triangle/src/TestTriangle.java
+++
@@ -1,7 +0,0 @@
-public class TestTriangle {
-    public static void main(String[] args) throws Exception {
-        double Side1 = 10;
-        double Side2 = 10;
-        double Side3 = 10;
-    }
-}
